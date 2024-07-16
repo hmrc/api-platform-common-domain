c8da5d65f933d0c7387822d1441aed4780d25f47
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-07-16 11:49:20 +0100

    APIS-7083 - Add times to fixed clock and tostring to LaxEmail

--- test-common/src/main/scala/uk/gov/hmrc/apiplatform/modules/common/utils/FixedClock.scala
+++ test-common/src/main/scala/uk/gov/hmrc/apiplatform/modules/common/utils/FixedClock.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.apiplatform.modules.common.utils
 
+import java.time.temporal.ChronoUnit
 import java.time.{Clock, Instant, LocalDateTime, ZoneOffset}
 
 import uk.gov.hmrc.apiplatform.modules.common.services.{ClockNow, DateTimeHelper}
@@ -28,11 +29,31 @@ trait FixedClock extends ClockNow with DateTimeHelper {
 
   override val instant: Instant = now.toInstant(utc)
 
+  object Instants {
+    val fiveMinsAgo = instant.minus(5, ChronoUnit.MINUTES)
+    val anHourAgo   = instant.minus(1, ChronoUnit.HOURS)
+    val aDayAgo     = instant.minus(1, ChronoUnit.DAYS)
+    val aWeekAgo    = instant.minus(7, ChronoUnit.DAYS)
+    val aMonthAgo   = instant.minus(31, ChronoUnit.DAYS)
+    val aYearAgo    = instant.minus(365, ChronoUnit.DAYS)
+  }
+
+  object Texts {
+    val now         = "2020-01-02T03:04:05.006Z"
+    val fiveMinsAgo = "2020-01-02T02:59:05.006Z"
+    val anHourAgo   = "2020-01-02T02:04:05.006Z"
+    val aDayAgo     = "2020-01-01T03:04:05.006Z"
+    val aWeekAgo    = "2019-12-26T03:04:05.006Z"
+    val aMonthAgo   = "2019-12-02T03:04:05.006Z"
+    val aYearAgo    = "2019-01-02T03:04:05.006Z"
+  }
+
   override val precise: Instant = instant.plusNanos(7008)
 
   val nowAsText: String = "2020-01-02T03:04:05.006Z"
 
   val clock: Clock = Clock.fixed(instant, utc)
+
 }
 
 object FixedClock extends FixedClock
