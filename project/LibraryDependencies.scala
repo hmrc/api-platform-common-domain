6a5879196c32afda3f9e81c3495391fcdb87011d
kskourtis1 <kskourtis1@users.noreply.github.com>
2025-09-05 14:34:40 +0100

    APIS-7743: Confirm Link of App to Org (#22)
    * APIS-7743: Confirm Link of App to Org
    
    * APIS-7743: Confirm Link of App to Org - bumped versions

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -7,13 +7,13 @@ object LibraryDependencies {
     
     lazy val compileDependencies = Seq(
       "org.playframework"       %% "play-json"                      % "3.0.1",
-      "uk.gov.hmrc"             %% "play-json-union-formatter"      % "1.21.0",
+      "uk.gov.hmrc"             %% "play-json-union-formatter"      % "1.22.0",
       "org.typelevel"           %% "cats-core"                      % "2.10.0"
     )
 
     lazy val testDependencies = Seq(
       "com.vladsch.flexmark"     % "flexmark-all"                   % "0.62.2",
-      "org.mockito"             %% "mockito-scala-scalatest"        % "1.17.29",
+      "org.mockito"             %% "mockito-scala-scalatest"        % "2.0.0",
       "org.scalatest"           %% "scalatest"                      % "3.2.17"
     )
 }
