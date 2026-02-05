8b02b499f175c974c7a06d55b9d8d21a14cd569a
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2026-02-05 15:49:17 +0000

    Moving Environment model to scala 3

---
+++ api-platform-common-domain/src/main/scala-3/uk/gov/hmrc/apiplatform/modules/common/domain/models/Environment.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.common.domain.models
+
+import uk.gov.hmrc.apiplatform.modules.common.domain.services.SealedTraitJsonFormatting
+
+enum Environment:
+  case Production, Sandbox
+
+
+// sealed trait Environment {
+//   final lazy val isSandbox: Boolean    = this == Environment.SANDBOX
+//   final lazy val isProduction: Boolean = !isSandbox
+//   final lazy val displayText: String   = Environment.displayText(this)
+// }
+
+object Environment {
+  // case object PRODUCTION extends Environment
+  // case object SANDBOX    extends Environment
+
+  // val values = Set[Environment](PRODUCTION, SANDBOX) // enum already has a values method which returns an Array[Environment]
+
+  import cats.implicits._
+
+  val displayText: Environment => String = _ match {
+    case Production => "Production"
+    case _          => "Sandbox"
+  }
+
+  def apply(text: String): Option[Environment] = text.toUpperCase match {
+    case "PRODUCTION" => Production.some
+    case "SANDBOX"    => Sandbox.some
+    case _            => None
+  }
+
+  def unsafeApply(text: String): Environment = {
+    apply(text).getOrElse(throw new RuntimeException(s"$text is not a valid Environment"))
+  }
+
+  import play.api.libs.json.Format
+
+  given Format[Environment] = SealedTraitJsonFormatting.createFormatFor[Environment]("Environment", apply(_))
+}
