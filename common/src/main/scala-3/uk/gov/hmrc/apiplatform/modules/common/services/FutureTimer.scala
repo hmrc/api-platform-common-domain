6c214b8e94a51293cc131c9e2abb3b308180efb6
John Green <john.green@digital.hmrc.gov.uk>
2026-07-28 10:47:47 +0100

    API-9208: PR bot recommendations

--- common/src/main/scala-3/uk/gov/hmrc/apiplatform/modules/common/services/FutureTimer.scala
+++ common/src/main/scala-3/uk/gov/hmrc/apiplatform/modules/common/services/FutureTimer.scala
@@ -23,11 +23,9 @@ trait FutureTimer {
   self: ClockNow =>
 
   def timeThisFuture[T](f: => Future[T])(using ExecutionContext): Future[TimedValue[T]] = {
-    println("Starting timer") // No logger in scope in project
     val startTime: Instant = precise
 
     f.map(value => {
-      println("Mapping") // No logger in scope in project
       val endTime: Instant = precise
       val duration         = Duration.between(startTime, endTime)
       TimedValue(value, duration)
